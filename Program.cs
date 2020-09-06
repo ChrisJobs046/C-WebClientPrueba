460ac45a3006a4380150afc31cbad005eedafc99
ChrisJobs046 <ChrisJobs046@gmail.com>
2020-09-06 13:57:22 -0400

    cool

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+
+namespace C_WebClientPrueba
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Console.Write("Bienvenido a obtener datos de la URL");
+
+            Program program = new Program();
+            await program.GetTodo();
+            
+        }
+        
+
+        private async Task GetTodo(){
+            //string URL = Console.ReadLine();
+            string response = await client.GetStringAsync("https://localhost:44319/api/productos");
+            Console.WriteLine(response);
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # C-WebClientPrueba
+# Este es una prueba, para saber como podemos recibir peticiones http desde c# y la forma en la cual podemos manejarlas
